b6e96714eb96f6cd5007fa8ead7b732f2146bd7a
Jordan <jordanmanu@live.com.au>
2022-05-02 15:57:08 +0100

    Example 2 added and completed, added paragraph to browser

---
+++ example-2/view.js
@@ -0,0 +1,18 @@
+class View {
+  constructor() {
+    this.mainContainerEl = document.querySelector('#main-container');
+
+    console.log(this.mainContainerEl);
+
+}
+
+addParagraph() {
+  let selectElement = document.querySelector("#main-container")
+  let p = document.createElement("p")
+  p.innerText = "This paragraph has been dynamically added by JavaScript!"
+  selectElement.append(p)
+  console.log(selectElement.childNodes)
+}
+}
+
+module.exports = View;
