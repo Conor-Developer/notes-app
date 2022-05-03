d6ca4c33c0bd9c780ce1aae9876856f67564d5fb
Conor Hope <c_hope@hotmail.co.uk>
2022-05-03 13:11:15 +0100

    Completed chapter 6 - phase two

--- example-2/view.test.js
+++ example-2/view.test.js
@@ -2,15 +2,28 @@
  * @jest-environment jsdom
  */
 
-const fs = require('fs');
-const View = require('./view');
+const fs = require("fs");
+const View = require("./view");
 
-describe('Page view', () => {
-  it('displays 2 paragraphs', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
+describe("Page view", () => {
+  beforeEach(() => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
+    view = new View();
+  });
 
-    const view = new View();
+  it("displays 2 paragraphs", () => {
+    expect(document.querySelectorAll("p").length).toBe(2);
+  });
 
-    expect(document.querySelectorAll('p').length).toBe(2);
+  it("#addParagraph adds 1 paragraph and displays on page", () => {
+    view.addParagraph();
+
+    expect(document.querySelectorAll("p").length).toBe(3);
+  });
+
+  it("#clearParagraphs removes all paragraphs displayed on page", () => {
+    view.clearParagraphs();
+
+    expect(document.querySelectorAll("p").length).toBe(0);
   });
 });
