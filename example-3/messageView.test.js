e702cd1c56bab475f118383f00bed64b9bbc9738
Conor Hope <c_hope@hotmail.co.uk>
2022-05-03 17:41:16 +0100

    Completed number 9 - Button accepts user text input

--- example-3/messageView.test.js
+++ example-3/messageView.test.js
@@ -2,30 +2,33 @@
  * @jest-environment jsdom
  */
 
-const fs = require('fs');
-const MessageView = require('./messageView');
+const fs = require("fs");
+const MessageView = require("./messageView");
 
-describe('MessageView', () => {
-  it('clicks the button', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
+describe("MessageView", () => {
+  it("clicks the button", () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
 
     const view = new MessageView();
 
-    const buttonEl = document.querySelector('#show-message-button');
+    const inputEL = document.querySelector("#message-input");
+    inputEL.value = "Hello World";
+
+    const buttonEl = document.querySelector("#show-message-button");
     buttonEl.click();
 
-    expect(document.querySelector('#message')).not.toBeNull();
+    expect(document.querySelector("#message").innerText).toBe("Hello World");
   });
-  it('Hides the message after button is clicked', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
+  it("Hides the message after button is clicked", () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
 
     const view = new MessageView();
 
-    const showButtonEl = document.querySelector('#show-message-button');
+    const showButtonEl = document.querySelector("#show-message-button");
     showButtonEl.click();
-    const buttonEl = document.querySelector('#hide-message-button');
+    const buttonEl = document.querySelector("#hide-message-button");
     buttonEl.click();
 
-    expect(document.querySelector('#message')).toBeNull();
-  })
+    expect(document.querySelector("#message")).toBeNull();
+  });
 });
